998cd624b346b651589b357dcc326d09f603fa3e
Anna <anutochkka@mail.ru>
2014-04-17 05:21:20 -0400

    Signed-off-by: Anna <anutochkka@mail.ru>

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int main(int argc, char **argv)
     fgets(first, 255, stdin);
     first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
-    printf("Now enter your last name: ");
+    printf("No enter your last name: ");
     gets(last); /* buffer overflow? what's that? */
 
     printf("Helo %s %s!\n", first, last);
